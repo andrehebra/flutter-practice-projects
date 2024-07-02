7cddeaadb3102d51393b22f8d3726ceffd84a0c1
andrehebra <andrehebra@gmail.com>
2024-07-02 13:08:58 -0400

    added drawer

--- demo_application/lib/pages/first_page.dart
+++ demo_application/lib/pages/first_page.dart
@@ -4,12 +4,33 @@ import 'package:flutter/material.dart';
 class FirstPage extends StatelessWidget {
   const FirstPage({super.key});
 
+  List<Widget> createListWidgets(int num) {
+    List<Widget> returnList = [];
+
+    for (int i = 0; i < num; i++) {
+      returnList.add(ListTile(
+        leading: Icon(Icons.delete),
+        subtitle: Text("SUBTITLE"),
+        title: Text("title"),
+        trailing: Text(i.toString()),
+        onTap: () {},
+      ));
+    }
+
+    return returnList;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("First Page"),
       ),
+      drawer: Drawer(
+          backgroundColor: Colors.blue,
+          child: ListView(
+            children: createListWidgets(5000),
+          )),
       body: Center(
           child: ElevatedButton(
         child: const Text("navigate to second page"),
